a1c35b20d09c8da5adee5e0981222bc74fc7f785
Takuo WATANABE <takuo@acm.org>
2018-05-18 12:34:48 +0900

    First runnable version

--- app/src/main/java/jp/ac/titech/itpro/sdl/simplelocation/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/simplelocation/MainActivity.java
@@ -1,13 +1,116 @@
 package jp.ac.titech.itpro.sdl.simplelocation;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+    private final static String TAG = "MainActivity";
+
+    private TextView latLongView;
+
+    private GoogleApiClient googleApiClient;
+    private FusedLocationProviderClient fusedLocationClient;
+
+    private final static String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+    private final static int REQCODE_PERMISSIONS = 1111;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+        latLongView = findViewById(R.id.latlong_view);
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+        googleApiClient.connect();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+        googleApiClient.disconnect();
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.d(TAG, "onConnected");
+        showLastLocation(true);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.d(TAG, "onConnectionSuspented");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.d(TAG, "onConnectionFailed");
+    }
+
+    private void showLastLocation(boolean reqPermission) {
+        for (String permission : PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                if (reqPermission)
+                    ActivityCompat.requestPermissions(this, PERMISSIONS, REQCODE_PERMISSIONS);
+                else
+                    Toast.makeText(this, getString(R.string.toast_requires_permission, permission), Toast.LENGTH_LONG).show();
+                return;
+            }
+        }
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        latLongView.setText(getString(R.string.latlong_format,
+                                location.getLatitude(), location.getLongitude()));
+                    }
+                });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int reqCode,
+                                           @NonNull String[] permissions, @NonNull int[] grants) {
+        Log.d(TAG, "onRequestPermissionsResult");
+        switch (reqCode) {
+        case REQCODE_PERMISSIONS:
+            showLastLocation(false);
+            break;
+        }
     }
 }
