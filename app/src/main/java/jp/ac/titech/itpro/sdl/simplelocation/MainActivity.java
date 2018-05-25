c5021660c36f651b445cd236b6476ae1564dbf6c
Takuo WATANABE <takuo@acm.org>
2018-05-25 10:34:49 +0900

    Removed unused import

--- app/src/main/java/jp/ac/titech/itpro/sdl/simplelocation/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/simplelocation/MainActivity.java
@@ -20,7 +20,6 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks,
